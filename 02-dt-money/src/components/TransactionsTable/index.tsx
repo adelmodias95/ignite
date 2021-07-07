2536fa1b7092054dd84d841ea79c5ad81656c1cc
adelmo-mzgroup <dev1@mzgroup.com>
2021-07-07 15:44:12 -0300

    libs: mirage js and axios

--- 02-dt-money/src/components/TransactionsTable/index.tsx
+++ 02-dt-money/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
-import { Container } from "./styles";
+import { useEffect } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles';
 
 export function TransactionsTable() {
+    useEffect(() => {
+        api.get('transactions')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <Container>
             <table>
@@ -14,14 +21,14 @@ export function TransactionsTable() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td className="title">Desenvolvimento de website</td>
-                        <td className="deposit">+ R$ 12.000,00</td>
+                        <td className='title'>Desenvolvimento de website</td>
+                        <td className='deposit'>+ R$ 12.000,00</td>
                         <td>Desenvolvimento</td>
                         <td>20/02/2021</td>
                     </tr>
                     <tr>
-                        <td className="title">Aluguel Escritório</td>
-                        <td className="withdraw">- R$ 1.100,00</td>
+                        <td className='title'>Aluguel Escritório</td>
+                        <td className='withdraw'>- R$ 1.100,00</td>
                         <td>Casa</td>
                         <td>10/02/2021</td>
                     </tr>
--- 02-dt-money/src/index.tsx
+++ 02-dt-money/src/index.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createServer } from 'miragejs'
+import { App } from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Desenvolvimento de website',
+          amount: 12000,
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Aluguel Escritório',
+          amount: 1100,
+          type: 'withdraw',
+          category: 'Casa',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
