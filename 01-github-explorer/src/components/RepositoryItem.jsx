5b8d145a2e4bd2edea513ca0219e85f14e94ee52
adelmodias <contato@adelmo.dev>
2021-10-06 23:11:27 -0300

    Counter deleted

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem(props) {
-    return (
-        <li>
-            <strong>{props.repository?.name ?? 'No name'}</strong>
-            <p>{props.repository?.description ?? 'No description'}</p>
-            <a href={props.repository?.link ?? '#'}>Acessar repositório</a>
-        </li>
-    );
+  return (
+    <li>
+      <strong>{props.repository?.name ?? 'No name'}</strong>
+      <p>{props.repository?.description ?? 'No description'}</p>
+      <a href={props.repository?.link ?? '#'}>Acessar repositório</a>
+    </li>
+  );
 }
